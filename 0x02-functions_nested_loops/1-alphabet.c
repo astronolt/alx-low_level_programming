6b565c4b83c978a5ea7b71be2f9c7f62891250e0
Emmanuel Odinaka <odinaka.dev@gmail.com>
2023-02-21 17:37:20 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - writes the alphabet to stdout
+ *
+ */
+void print_alphabet(void)
+{
+	int i = 0;
+	int alphaSize = 26;
+	char str[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	while (i < alphaSize)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}

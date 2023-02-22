b888485d46721b622ff085a47220ee01e2747523
Emmanuel Odinaka <odinaka.dev@gmail.com>
2023-02-21 20:02:09 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,8 @@
+
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +16,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
+
+
+#endif

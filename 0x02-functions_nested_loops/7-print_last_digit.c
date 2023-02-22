bf3646f4dc46c4acb37723d545b8da6afa7b4434
Emmanuel Odinaka <odinaka.dev@gmail.com>
2023-02-21 18:24:10 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of a number to stdout
+ * @n: Number to compute from
+ *
+ * Return: Last digit from n
+ */
+int print_last_digit(int n)
+{
+	int x;
+
+	x = n % 10;
+	if (x < 0)
+		x = -x;
+	_putchar(x + '0');
+
+	return (x);
+}

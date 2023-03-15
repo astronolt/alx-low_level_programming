3df7bec6ee2c0a6a97c16eb99a97d42afe3e47b9
Emmanuel Odinaka <odinaka.dev@gmail.com>
2023-03-14 20:46:29 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - creates a duplicate string
+ *
+ * @str: input string to duplicate
+ * Return: a pointer to duplicate string
+ *
+ */
+char *_strdup(char *str)
+{
+	char *new;
+	int i, strlen;
+
+	if (str == NULL)
+		return (NULL);
+	strlen = 0;
+	while (str[strlen] != '\0')
+		strlen++;
+	new = malloc(sizeof(char) * (strlen + 1));
+	if (new == NULL)
+		return (NULL);
+	for (i = 0; i < strlen + 1; i++)
+		*(new + i) = *(str + i);
+	return (new);
+}

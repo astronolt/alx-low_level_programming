20f2c19b020756b8e3dfa77f9e46681a38b3fcf5
Emmanuel Odinaka <odinaka.dev@gmail.com>
2023-03-23 16:41:55 -0700

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,19 +1,19 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef _FUNCTION_POINTERS_H
+#define _FUNCTION_POINTERS_H
 
+#include <stdio.h>
+#include <stdio.h>
+
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
-/**
- * enum bool - typedef function for bool
- * @TRUE: 1
- * @FALSE: 0
-*/
-typedef enum bool
-{
-	TRUE = 1,
-	FALSE = 0
-} bool;
 
-#endif /* FUNCTION_POINTERS_H */
+#endif
